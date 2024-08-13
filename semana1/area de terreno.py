c71a22887786aae8c4cf058cad696417492679a3
EdwarBenito <edwar.benitobas@campusucc.edu.co>
2024-08-13 16:52:15 -0500

    Ejercicios semana 1

---
+++ semana1/area de terreno.py
@@ -0,0 +1,8 @@
+print("Bienvenido al programa de la empresa para calcular el area del terreno")
+base = float(input("ponga la base del terreno en mts: "))
+altura_triangulo  = float(input("ponga la altura del triangulo en mts: "))
+altura_rectangulo = float(input("ponga la altura del rectangulo en mts: "))
+area_triangulo = base * altura_triangulo / 2
+area_rectangulo = base * altura_rectangulo
+area_total = area_rectangulo + area_triangulo
+print("el area total del terreno es: ", area_total," mts")
